b4187c66140e9d925a2652c6fb734d95e659bb2b
viktasidenfy <78145743+viktasidenfy@users.noreply.github.com>
2024-11-19 09:06:07 +0200

    Add files via upload

---
+++ Package.swift
@@ -0,0 +1,124 @@
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let version = "8.5.7"
+
+enum Checksums {
+    static let iDenfyInternalLoggerChecksum = "3948e0d6d5374a4e747b8554a27ed8d46dca51c43c2ba78284494edef0638b53"
+    static let FaceTecSDKChecksum = "9e79b55573e1ab67771f0c9d43cdec58ddc78887de23f93835972c06f280751b"
+    static let iDenfyLivenessChecksum = "9fcf13d562745866e750309942830982267005d9c00670dc00eb7794fe721b2a"
+    static let idenfyviewsChecksum = "9890a5cdb26a8df98027ccd5345dc1147f71feef9202e8e0cd6d8f82a4b3ae17"
+    static let iDenfySDKChecksum = "b1309fc6483c88347c50c1dd47733ce56e7d281450a614948729a496bf5de17f"
+    static let idenfycoreChecksum = "1dc38dde132b968dd6def89ee73d501f3a91cf2ece9a93921a0f2bbcc7be9a45"
+}
+
+let package = Package(
+    name: "iDenfyLiveness",
+    platforms: [.iOS(.v13)],
+    products: [
+        .library(
+            name: "iDenfyLiveness-Dynamic",
+            type: .dynamic,
+            targets: ["iDenfySDKTarget"]),
+        .library(
+            name: "iDenfyLiveness",
+            targets: ["iDenfySDKTarget"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/airbnb/lottie-spm.git", "4.4.3"..<"4.4.4"),
+    ],
+    targets: [
+        //IdenfyViews
+        .target(
+            name: "idenfyviewsTarget",
+            dependencies: [.target(name: "idenfyviewsWrapper",
+                                   condition: .when(platforms: [.iOS]))],
+            path: "SwiftPM-PlatformExclude/idenfyviewsWrap"
+        ),
+        .target(
+            name: "idenfyviewsWrapper",
+            dependencies: [
+                .target(
+                    name: "idenfyviews",
+                    condition: .when(platforms: [.iOS])
+                ),
+                .target(name: "idenfycore",
+                        condition: .when(platforms: [.iOS])),
+                .product(name: "Lottie",
+                         package: "lottie-spm",
+                         condition: .when(platforms: [.iOS])),
+            ],
+            path: "idenfyviewsWrapper"
+        ),
+        //IdenfyLiveness
+        .target(
+            name: "IdenfyLivenessTarget",
+            dependencies: [.target(name: "IdenfyLivenessWrapper",
+                                   condition: .when(platforms: [.iOS]))],
+            path: "SwiftPM-PlatformExclude/IdenfyLivenessWrap"
+        ),
+        .target(
+            name: "IdenfyLivenessWrapper",
+            dependencies: [
+                .target(
+                    name: "IdenfyLiveness",
+                    condition: .when(platforms: [.iOS])
+                ),
+                .target(name: "FaceTecSDK",
+                        condition: .when(platforms: [.iOS])),
+                .target(name: "idenfyviewsTarget",
+                        condition: .when(platforms: [.iOS])),
+                .target(name: "idenfycore",
+                        condition: .when(platforms: [.iOS])),
+            ],
+            path: "IdenfyLivenessWrapper"
+        ),
+        //iDenfySDK
+        .target(
+            name: "iDenfySDKTarget",
+            dependencies: [.target(name: "iDenfySDKWrapper",
+                                   condition: .when(platforms: [.iOS]))],
+            path: "SwiftPM-PlatformExclude/iDenfySDKWrap",
+            cSettings: [
+                .define("CLANG_MODULES_AUTOLINK", to: "YES"),
+            ]
+        ),
+        .target(
+            name: "iDenfySDKWrapper",
+            dependencies: [
+                .target(
+                    name: "iDenfySDK",
+                    condition: .when(platforms: [.iOS])),
+                .product(name: "Lottie",
+                         package: "lottie-spm",
+                         condition: .when(platforms: [.iOS])),
+                .target(name: "idenfycore",
+                        condition: .when(platforms: [.iOS])),
+                .target(name: "iDenfyInternalLogger",
+                        condition: .when(platforms: [.iOS])),
+                .target(name: "idenfyviewsTarget",
+                        condition: .when(platforms: [.iOS])),
+                .target(name: "FaceTecSDK",
+                        condition: .when(platforms: [.iOS])),
+                .target(name: "IdenfyLivenessTarget",
+                        condition: .when(platforms: [.iOS])),
+            ],
+            path: "iDenfySDKWrapper"
+        ),
+        // Binaries
+        .binaryTarget(name: "iDenfyInternalLogger",
+                      url: "https://s3.eu-west-1.amazonaws.com/sdk.builds/ios-sdk/\(version)/spm/IdenfyLiveness/iDenfyInternalLogger.zip", checksum: Checksums.iDenfyInternalLoggerChecksum),
+        .binaryTarget(name: "FaceTecSDK",
+                      url: "https://s3.eu-west-1.amazonaws.com/sdk.builds/ios-sdk/\(version)/spm/IdenfyLiveness/FaceTecSDK.zip", checksum: Checksums.FaceTecSDKChecksum),
+        .binaryTarget(name: "IdenfyLiveness",
+                      url: "https://s3.eu-west-1.amazonaws.com/sdk.builds/ios-sdk/\(version)/spm/IdenfyLiveness/IdenfyLiveness.zip", checksum: Checksums.iDenfyLivenessChecksum),
+        .binaryTarget(name: "idenfyviews",
+                      url: "https://s3.eu-west-1.amazonaws.com/sdk.builds/ios-sdk/\(version)/spm/IdenfyLiveness/idenfyviews.zip", checksum: Checksums.idenfyviewsChecksum),
+        .binaryTarget(name: "iDenfySDK",
+                      url: "https://s3.eu-west-1.amazonaws.com/sdk.builds/ios-sdk/\(version)/spm/IdenfyLiveness/iDenfySDK.zip", checksum: Checksums.iDenfySDKChecksum),
+        .binaryTarget(name: "idenfycore",
+                      url: "https://s3.eu-west-1.amazonaws.com/sdk.builds/ios-sdk/\(version)/spm/IdenfyLiveness/idenfycore.zip", checksum: Checksums.idenfycoreChecksum),
+    ]
+)
