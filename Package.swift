8a656fd5322fe4142f743b0958de681dce3b21b6
viktasidenfy <78145743+viktasidenfy@users.noreply.github.com>
2024-11-19 09:12:56 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -12,6 +12,7 @@ enum Checksums {
     static let idenfyviewsChecksum = "9890a5cdb26a8df98027ccd5345dc1147f71feef9202e8e0cd6d8f82a4b3ae17"
     static let iDenfySDKChecksum = "b1309fc6483c88347c50c1dd47733ce56e7d281450a614948729a496bf5de17f"
     static let idenfycoreChecksum = "1dc38dde132b968dd6def89ee73d501f3a91cf2ece9a93921a0f2bbcc7be9a45"
+    static let iDenfyBlurGlareDetectionChecksum = "1dc38dde132b968dd6def89ee73d501f3a91cf2ece9a93921a0f2bbcc7be9a45"
 }
 
 let package = Package(
@@ -30,6 +31,23 @@ let package = Package(
         .package(url: "https://github.com/airbnb/lottie-spm.git", "4.4.3"..<"4.4.4"),
     ],
     targets: [
+        //iDenfyBlurGlareDetection
+        .target(
+            name: "iDenfyBlurGlareDetectionTarget",
+            dependencies: [.target(name: "iDenfyBlurGlareDetectionWrapper",
+                                   condition: .when(platforms: [.iOS]))],
+            path: "SwiftPM-PlatformExclude/iDenfyBlurGlareDetectionWrap"
+        ),
+        .target(
+            name: "iDenfyBlurGlareDetectionWrapper",
+            dependencies: [
+                .target(
+                    name: "iDenfyBlurGlareDetection",
+                    condition: .when(platforms: [.iOS])
+                )
+            ],
+            path: "iDenfyBlurGlareDetectionWrapper"
+        ),
         //IdenfyViews
         .target(
             name: "idenfyviewsTarget",
@@ -104,21 +122,25 @@ let package = Package(
                         condition: .when(platforms: [.iOS])),
                 .target(name: "IdenfyLivenessTarget",
                         condition: .when(platforms: [.iOS])),
+                .target(name: "iDenfyBlurGlareDetectionTarget",
+                        condition: .when(platforms: [.iOS]))
             ],
             path: "iDenfySDKWrapper"
         ),
         // Binaries
         .binaryTarget(name: "iDenfyInternalLogger",
-                      url: "https://s3.eu-west-1.amazonaws.com/sdk.builds/ios-sdk/\(version)/spm/IdenfyLiveness/iDenfyInternalLogger.zip", checksum: Checksums.iDenfyInternalLoggerChecksum),
+                      url: "https://s3.eu-west-1.amazonaws.com/prod-ivs-sdk.builds/ios-sdk/\(version)/spm/iDenfyBlurGlareDetection/iDenfyInternalLogger.zip", checksum: Checksums.iDenfyInternalLoggerChecksum),
         .binaryTarget(name: "FaceTecSDK",
-                      url: "https://s3.eu-west-1.amazonaws.com/sdk.builds/ios-sdk/\(version)/spm/IdenfyLiveness/FaceTecSDK.zip", checksum: Checksums.FaceTecSDKChecksum),
+                      url: "https://s3.eu-west-1.amazonaws.com/prod-ivs-sdk.builds/ios-sdk/\(version)/spm/iDenfyBlurGlareDetection/FaceTecSDK.zip", checksum: Checksums.FaceTecSDKChecksum),
         .binaryTarget(name: "IdenfyLiveness",
-                      url: "https://s3.eu-west-1.amazonaws.com/sdk.builds/ios-sdk/\(version)/spm/IdenfyLiveness/IdenfyLiveness.zip", checksum: Checksums.iDenfyLivenessChecksum),
+                      url: "https://s3.eu-west-1.amazonaws.com/prod-ivs-sdk.builds/ios-sdk/\(version)/spm/iDenfyBlurGlareDetection/IdenfyLiveness.zip", checksum: Checksums.iDenfyLivenessChecksum),
         .binaryTarget(name: "idenfyviews",
-                      url: "https://s3.eu-west-1.amazonaws.com/sdk.builds/ios-sdk/\(version)/spm/IdenfyLiveness/idenfyviews.zip", checksum: Checksums.idenfyviewsChecksum),
+                      url: "https://s3.eu-west-1.amazonaws.com/prod-ivs-sdk.builds/ios-sdk/\(version)/spm/iDenfyBlurGlareDetection/idenfyviews.zip", checksum: Checksums.idenfyviewsChecksum),
         .binaryTarget(name: "iDenfySDK",
-                      url: "https://s3.eu-west-1.amazonaws.com/sdk.builds/ios-sdk/\(version)/spm/IdenfyLiveness/iDenfySDK.zip", checksum: Checksums.iDenfySDKChecksum),
+                      url: "https://s3.eu-west-1.amazonaws.com/prod-ivs-sdk.builds/ios-sdk/\(version)/spm/iDenfyBlurGlareDetection/iDenfySDK.zip", checksum: Checksums.iDenfySDKChecksum),
         .binaryTarget(name: "idenfycore",
-                      url: "https://s3.eu-west-1.amazonaws.com/sdk.builds/ios-sdk/\(version)/spm/IdenfyLiveness/idenfycore.zip", checksum: Checksums.idenfycoreChecksum),
+                      url: "https://s3.eu-west-1.amazonaws.com/prod-ivs-sdk.builds/ios-sdk/\(version)/spm/iDenfyBlurGlareDetection/idenfycore.zip", checksum: Checksums.idenfycoreChecksum),
+        .binaryTarget(name: "iDenfyBlurGlareDetection",
+                      url: "https://s3.eu-west-1.amazonaws.com/prod-ivs-sdk.builds/ios-sdk/\(version)/spm/iDenfyBlurGlareDetection/iDenfyBlurGlareDetection.zip", checksum: Checksums.iDenfyBlurGlareDetectionChecksum),
     ]
 )
